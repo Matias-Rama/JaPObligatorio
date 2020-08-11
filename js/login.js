fd145de4c961fb8578e406cf170675f0aa59d623
Matias Rama <matias.rama@fing.edu.uy>
2020-08-11 17:17:05 -0300

    Fixes

--- js/login.js
+++ js/login.js
@@ -2,11 +2,21 @@
 function onSuccess(googleUser)
 {
    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+   window.location.assign("index.html");
 }
 
 function onFailure(error)
 {
    console.log(error);
+}
+
+function signOut()
+{
+   var auth2 = gapi.auth2.getAuthInstance();
+   auth2.signOut().then(function()
+   {
+      console.log('User signed out.');
+   });
 }
 
 function renderButton()
