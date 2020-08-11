57e233c123bb9bb263561b880ee4a9e40cf2123d
Matias Rama <matias.rama@fing.edu.uy>
2020-08-11 17:42:14 -0300

    Fix

--- js/login.js
+++ js/login.js
@@ -12,8 +12,7 @@ function onFailure(error)
 
 function signOut()
 {
-   var auth2 = gapi.auth2.getAuthInstance();
-   auth2.signOut().then(function()
+   gapi.auth2.getAuthInstance().auth2.signOut().then(function()
    {
       console.log('User signed out.');
    });
