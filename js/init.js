0b799b996631a726d7d09f9f736ea089af31d8c2
Matias Rama <matias.rama@fing.edu.uy>
2020-08-14 15:22:50 -0300

    s

--- js/init.js
+++ js/init.js
@@ -42,14 +42,15 @@ var getJSONData = function(url){
 
 function signOut()
 {
-  gapi.auth2.getAuthInstance().auth2.signOut().then(function () {
+  /* var auth2 = gapi.auth2.getAuthInstance(); */
+  auth2.signOut().then(function () {
     console.log('User signed out.');
   });
 }
 
 function onLoad() {
   gapi.load('auth2', function() {
-    gapi.auth2.init();
+    var auth2 = gapi.auth2.init();
   });
 }
 
