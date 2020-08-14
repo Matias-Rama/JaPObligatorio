e617e8f15f01345cf0d8634deb868224eb334f84
Matias Rama <matias.rama@fing.edu.uy>
2020-08-14 13:23:46 -0300

    ccc

--- js/login.js
+++ js/login.js
@@ -27,8 +27,7 @@ function renderButton()
 
 function signOut()
 {
-   var auth2 = gapi.auth2.getAuthInstance();
-   auth2.signOut().then(function () {
+   gapi.auth2.getAuthInstance().auth2.signOut().then(function () {
       console.log('User signed out.');
    });
 }
